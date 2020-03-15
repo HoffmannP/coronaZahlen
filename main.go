5cacd5e808e3a5d1d088c2df9141cc3216c7b1eb
Berengar W. Lehr <Berengar.Lehr@kompetenztest.de>
2020-03-15 03:17:53 +0100

    Reihnfolge korrigiert

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
 func main() {
 	header := []string{"Bundesland", "Fälle", "RKI", "URL"}
@@ -8,7 +11,16 @@ func main() {
 
 	rki := loadRKI()
 	sum := 0
-	for regionName, regionData := range updateURLs(regions()) {
+
+	regions := updateURLs(regions())
+	var regionNames []string
+	for regionName := range regions {
+		regionNames = append(regionNames, regionName)
+	}
+	sort.Strings(regionNames)
+
+	for _, regionName := range regionNames {
+		regionData := regions[regionName]
 		var line []string
 		line = append(line, regionName)
 
@@ -28,10 +40,11 @@ func main() {
 		}
 
 		line = append(line, "<a href=\""+regionData.URL+"\">Quelle</a>")
+		println("append", regionName)
 		rows = append(rows, line)
 	}
 
 	footer := []string{"Deutschland", strconv.Itoa(sum), strconv.Itoa(rki.lookup("Gesamt")), "<a href=\"" + rki.url + "\">Quelle</a>"}
 
-	display(header, rows, footer, "dist/coronaZahl.html")
+	display(header, rows, footer, "docs/index.html")
 }
