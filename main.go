6b0c89e932504ac8ceeaf86c0cdeb1ee7da56b85
Berengar W. Lehr <Berengar.Lehr@kompetenztest.de>
2020-03-16 23:15:48 +0100

    paralleles Abfragen

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
+func getRegion(regionName string, regionData caseRegion, rki rkiType, j data, sum chan int) {
+	casecount, timestamp := regionData.loadRegion()
+	rkicount := rki.lookup(regionName)
+	var summand int
+
+	if casecount == -1 || rkicount > casecount {
+		summand = rkicount
+	} else {
+		summand = casecount
+	}
+
+	j.append(regionName, regionData.URL, timestamp, casecount, rkicount)
+
+	sum <- summand
+}
+
+func remaing(remaining int, sum *int, next chan int) {
+	for remaining > 0 {
+		*sum += <-next
+		remaining--
+	}
+}
+
 func main() {
 	rki := loadRKI()
 	sum := 0
 	data := newData("CoronaCountsGermany", rki)
+	allRegions := regions()
+	next := make(chan int)
 
-	for regionName, regionData := range regions() {
-		casecount, timestamp := regionData.loadRegion()
-		rkicount := rki.lookup(regionName)
-
-		if casecount == -1 || rkicount > casecount {
-			sum += rkicount
-		} else {
-			sum += casecount
-		}
-
-		data.append(
-			regionName,
-			regionData.URL, timestamp, casecount, rkicount)
+	for regionName, regionData := range allRegions {
+		go getRegion(regionName, regionData, rki, data, next)
 	}
+	remaing(len(allRegions), &sum, next)
 
 	data.sum(sum)
 	data.saveHTML("template.html", "docs/index.html")
